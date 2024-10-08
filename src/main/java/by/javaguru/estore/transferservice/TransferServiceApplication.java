58efa723aa9011217a28a3002b9e4707de057b7c
Igor Kukyan <kukyan@irc.beltelecom.by>
2024-10-08 11:32:50 +0300

    first

---
+++ src/main/java/by/javaguru/estore/transferservice/TransferServiceApplication.java
@@ -0,0 +1,13 @@
+package by.javaguru.estore.transferservice;
+
+import org.springframework.boot.SpringApplication;
+import org.springframework.boot.autoconfigure.SpringBootApplication;
+
+@SpringBootApplication
+public class TransferServiceApplication {
+
+    public static void main(String[] args) {
+        SpringApplication.run(TransferServiceApplication.class, args);
+    }
+
+}
